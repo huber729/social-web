2da36fad2267da122164935dfa70c4f623baf4d9
Collin Huber <collin722999@gmail.com>
2024-01-30 17:56:53 -0500

    Update route.ts

--- app/api/auth/[...nextauth]/route.ts
+++ app/api/auth/[...nextauth]/route.ts
@@ -2,7 +2,7 @@ import NextAuth, { type NextAuthOptions } from 'next-auth'
 import GoogleProvider from "next-auth/providers/google"
 import { prisma } from "../../../../lib/prisma"
 
-export const authOptions: NextAuthOptions = {
+const authOptions: NextAuthOptions = {
     session: {
       strategy: 'jwt'
     },
