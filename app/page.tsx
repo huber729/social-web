a603553ff8e4cf817898e8deed51ecb6797e47eb
Collin Huber <collin722999@gmail.com>
2024-01-31 17:47:36 -0500

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -3,7 +3,7 @@ import Link from "next/link"
 export default function Home() {
   return (
     <div>
-      <p>Welcome to my Post app. Please navigate to the feed by clicking the "Feed" button below!</p>
+      <p>Welcome to my Post app. Please navigate to the feed by clicking the "Feed" button below! If you are on a cellphone turn sideways.</p>
       <Link href="/feed">
         <h1 style={{ fontWeight: "bold" }}>Feed</h1>
       </Link>
